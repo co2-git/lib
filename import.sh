4900b7eef286a378de8c411f296fc9a00366b050
co2-git <hack.co2@gmail.com>
2013-04-30 18:54:10 +0200

    fix string substitution error

--- import.sh
+++ import.sh
@@ -1,22 +1,20 @@
 lib(){
-	local function="$1"
-	shift
-	local file=$libPATH/${function/./\///}.sh
-	local method="lib.$function"
+	local function="$1";
+	shift;
+	local file=$libPATH/${function//./\/}.sh;
+	local method="lib.$function";
 
 	#
 	#	If method already exists no need to require file
 
-	if type $method >/dev/null 2>&1
-		then
-		$method "$@"
-	elif [ -f "$file" ] 
-		then
-		. $file
-		
-		if type $method >/dev/null 2>&1
-			then
-			$method "$@"
+	if type $method >/dev/null 2>&1; then
+		$method "$@";
+	elif [ -f "$file" ]; then
+		. $file;
+		if type $method >/dev/null 2>&1; then
+			$method "$@";
 		fi
+	else
+		echo lib\: Function not found\: $function in $file
 	fi
 }
