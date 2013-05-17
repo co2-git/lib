256514a75961c6ade7b4ffd81c23614bcc8468ef
francois <you@example.com>
2013-05-17 20:40:03 +0200

    simplified controller

--- lib.sh
+++ lib.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
 [ -z "$lib_path" ] && {
-	[ -L "$0" ] && lib_path=$(dirname $(readlink "$0")) || 
-	lib_path=$(dirname "$0")
-}
-
+	lib_path="${BASH_SOURCE[0]:-$0}";
+	while [ -h "$lib_path" ]
+	do
+		lib_path="$(readlink "$lib_path")";
+	done
+	lib_path="$(dirname "$lib_path")";
+};
 [ ! -d "$lib_path" ] && [ ! "$1" = --help ] && {
     echo "lib_path not found! Try <lib --help> for help"
     exit
-}
+};
 
 lib_default=hello;
 . $lib_path/import.sh;
